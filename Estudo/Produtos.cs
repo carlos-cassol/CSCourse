050a46a92deafeafebf94dc8c9d96cd56ab3dc1e
carlo <carlo@CASSOL>
2022-12-25 20:12:08 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo/Produtos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo
+{
+    internal class Produtos
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quant { get; private set; }
+
+
+        public Produtos(string nome, int quant, double preco)
+        {
+            _nome = nome;
+            Quant = quant;
+            Preco = preco;
+        }
+
+        public Produtos(string nome)
+        {
+            _nome = nome;
+            Quant = 0;
+            Preco = 0;
+        }
+
+        public Produtos()
+        { 
+        }
+
+        public string Nome { 
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorEmEstoque() {
+            return Preco * Quant;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quant += quantidade;
+        }
+        public void RemoverProdutos(int quantidade){
+            Quant -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+            + ", $"
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quant
+            + " unidades, total: $"
+            + ValorEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Estudo/Program.cs
@@ -0,0 +1,72 @@
+﻿using Estudo;
+
+namespace Curso
+{
+    public class Program {
+        public static void Main(string[] args){
+            
+            Console.WriteLine("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            
+            Console.WriteLine("Digite a quantidade de produtos: ");
+            int quant = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o preço do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+        
+            Produtos p = new Produtos(nome, quant, preco);
+
+            Console.WriteLine(p.ToString());
+
+            Console.WriteLine("Deseja adicionar produtos? S para sim, N para não: ");
+            
+            string opt = Console.ReadLine();
+            opt = opt.ToUpper();
+            while (opt.ToUpper() != "S" || opt.ToUpper() != "N")
+            {
+                if (opt.ToUpper() == "S") { break; }
+                else
+                {
+                    Console.WriteLine("Opção incorreta. Favor prencher com S para sim e N para não");
+                    opt = Console.ReadLine();
+                    opt = opt.ToUpper();
+                }
+             }
+            if (opt == "S") 
+            {
+                Console.Write("Adicionar quantos produtos: ");
+                int qte = int.Parse(Console.ReadLine());
+                p.AdicionarProdutos(qte);
+                Console.WriteLine("Dados atualizados: ");
+                Console.Write(p.ToString());
+            }
+
+            Console.WriteLine("Deseja remover produtos? S para sim, N para não: ");
+
+            opt = Console.ReadLine().ToString().ToUpper();
+
+            while (opt.ToUpper() != "S" || opt.ToUpper() != "N")
+            {
+                if (opt.ToUpper() == "S") 
+                {
+                    Console.WriteLine("Digite a quantidade de produtos a serem removidos: ");
+                    quant = int.Parse(Console.ReadLine());
+                    p.RemoverProdutos(quant);
+                    Console.WriteLine("Dados atualizados: ");
+                    Console.Write(p.ToString());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opção incorreta. Favor prencher com S para sim e N para não");
+                    opt = Console.ReadLine();
+                    opt = opt.ToUpper();
+                }
+            }
+
+
+
+        }
+    }
+}
